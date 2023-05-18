63063be56d732b759389a1a129ab806353f60ce1
Abytova Saikal <saikalabytova1@gmail.com>
2023-05-18 16:44:38 +0600

    Set up function for the button and changed some css styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Circle from "./Circle/Circle";
 import './App.css';
 
+const App = () => {
+    const [circles, SetCircle] = useState([0, 0, 0, 0, 0]);
 
-const App = () => (
-    <div className="App">
-        <Circle num={0}/>
-        <Circle num={0}/>
-        <Circle num={0}/>
-        <Circle num={0}/>
-        <Circle num={0}/>
-    </div>
-);
+    const changeNum = () => {
+        let arr: number[] = [];
+
+        while(arr.length <= 4){
+            let randomNum = Math.floor(5 + Math.random() * ((36 - (5)) + 1));
+
+            if(arr.indexOf(randomNum) === -1){
+                arr.push(randomNum)
+            }
+        }
+
+        arr.sort((a,b) => {
+            return a - b;
+        });
+
+        SetCircle([arr[0], arr[1], arr[2], arr[3], arr[4]]);
+    };
+        return (
+            <div className="App">
+                <div className="circlesWrap">
+                    <Circle num={circles[0]}/>
+                    <Circle num={circles[1]}/>
+                    <Circle num={circles[2]}/>
+                    <Circle num={circles[3]}/>
+                    <Circle num={circles[4]}/>
+                </div>
+                <div className="btnWrap">
+                    <button onClick={changeNum} className="btn">New numbers</button>
+                </div>
+            </div>
+        );
+};
 
 export default App;
